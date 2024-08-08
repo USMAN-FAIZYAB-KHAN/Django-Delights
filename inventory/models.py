a2fd2113aead176352b6766515c137c9097c7872
USMAN-FAIZYAB-KHAN <usmanfaizyab123@gmail.com>
2024-08-08 18:10:31 +0500

    Updated Models

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 import datetime
 
-# Create your models here.
 class Ingredient(models.Model):
     """It represents a single ingredient in the restaurant's inventory"""
     name = models.CharField(max_length=100, unique=True)
@@ -25,10 +24,9 @@ class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField(default=0.0)
-    unit = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.menu_item.title} - {self.ingredient.name} - {self.quantity} {self.unit}"
+        return f"{self.menu_item.title} - {self.ingredient.name} - {self.quantity} {self.ingredient.unit}"
     
     class Meta:
         unique_together = ('menu_item', 'ingredient')
@@ -38,8 +36,24 @@ class Purchase(models.Model):
     timestamp = models.DateTimeField(default=datetime.datetime.now)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    total_cost_of_ingredients = models.FloatField(default=0.0)
+    # The price at which the menu item is sold to the customer
     unit_price = models.FloatField(default=0.0)
+    # The total price of the menu item sold to the customer
     total_price = models.FloatField(default=0.0)
+
+    def calculate_total_cost_of_ingredients(self):
+        """It calculates the total cost of the ingredients required to make the menu item at the time of purchase"""
+        total_cost = 0
+        for req in self.menu_item.reciperequirement_set.all():
+            total_cost += req.ingredient.unit_price * req.quantity
+        return total_cost
+    
+    def save(self, *args, **kwargs):
+        self.total_cost_of_ingredients = self.calculate_total_cost_of_ingredients()
+        self.unit_price = self.menu_item.price
+        self.total_price = self.unit_price * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.menu_item.title} - {self.quantity} - {self.timestamp}"
